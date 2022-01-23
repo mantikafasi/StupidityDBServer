67803d4e8401c13c1029e5f510878e2c42f33a20
Manti <67705577+mantikafasi@users.noreply.github.com>
2022-01-23 19:40:55 +0300

    Update mysqlconnection.py

--- mysqlconnection.py
+++ mysqlconnection.py
@@ -2,14 +2,15 @@ import mysql.connector
 from secrets import db,dbpw,dbip,dbuser
 class Manager:
     def __init__(self):
-        self.sql = mysql.connector.connect(host=dbip,user=dbuser,password=dbpw,database=db,autocommit=True)
-        self.cur = self.cursor()
-    
+        try:
+            self.sql = mysql.connector.connect(host=dbip,user=dbuser,password=dbpw,database=db,autocommit=True)
+            self.cur = self.cursor()
+        except Exception as e:
+            print(e)
     def cursor(self):
         try:
             self.sql.ping(reconnect=True, attempts=3, delay=5)
         except mysql.connector.Error:
-            self.sql.close()  
+            if self.sql != None : self.sql.close()  
             self.sql = mysql.connector.connect(host=dbip,user=dbuser,password=dbpw,database=db,autocommit=True)
-            self.cursor()
         return self.sql.cursor()
