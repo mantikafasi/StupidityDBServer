35af33f7650bf3e567d9902aa9c676fbb6c5c0d1
taskylizard <paltu.samal1234@gmail.com>
2022-01-31 21:34:57 +0530

    format

--- StupidityDBDCBot.py
+++ StupidityDBDCBot.py
@@ -2,35 +2,58 @@ from logging import Manager as LogManager
 import discord
 from threading import Thread
 from discord.ext import commands
-#from _mysqlManager import Manager,Vote
+
+# from _mysqlManager import Manager,Vote
 import json
 import requests
 from secrets import BOT_VOTE_TOKEN, token
+
 bot = commands.Bot(command_prefix=".")
+
 
 @bot.event
 async def on_ready():
     print(bot.user.name)
-    print('Olarak Giriş Yapıldı')
+    print("Olarak Giriş Yapıldı")
     print(bot.user.id)
-    print('------')
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Nothing"))    
+    print("------")
+    await bot.change_presence(
+        activity=discord.Activity(type=discord.ActivityType.watching, name="Nothing")
+    )
+
 
 @bot.event
 async def on_message(message):
     ctx = await bot.get_context(message)
-    mesaj=message.content.lower().strip()
+    mesaj = message.content.lower().strip()
     if mesaj.startswith("{"):
         obj = json.loads(mesaj)
         try:
             stupidity = int(obj["stupidity"])
-            if (stupidity >= 0 and stupidity <= 100 and (len(str(obj["discordid"]))<=19 and len(str(obj["discordid"]))>=17)):
-                await ctx.send(requests.post("https://mantikralligi1.pythonanywhere.com/putUser",json={"discordid":obj["discordid"],"stupidity":obj["stupidity"],"senderdiscordid":ctx.author.id,"token":BOT_VOTE_TOKEN}).text)
+            if (
+                stupidity >= 0
+                and stupidity <= 100
+                and (
+                    len(str(obj["discordid"])) <= 19
+                    and len(str(obj["discordid"])) >= 17
+                )
+            ):
+                await ctx.send(
+                    requests.post(
+                        "https://mantikralligi1.pythonanywhere.com/putUser",
+                        json={
+                            "discordid": obj["discordid"],
+                            "stupidity": obj["stupidity"],
+                            "senderdiscordid": ctx.author.id,
+                            "token": BOT_VOTE_TOKEN,
+                        },
+                    ).text
+                )
             else:
                 await ctx.send("An Error occured")
         except Exception as e:
             await ctx.send(e)
-            raise Exception (e)
+            raise Exception(e)
+
 
 bot.run(token)
-
--- mysqlconnection.py
+++ mysqlconnection.py
@@ -1,18 +1,26 @@
 import mysql.connector
-from _secrets import db,dbpw,dbip,dbuser
+from _secrets import db, dbpw, dbip, dbuser
+
+
 class Manager:
     def __init__(self):
         try:
             print("creating new connection")
-            self.sql = mysql.connector.connect(host=dbip,user=dbuser,password=dbpw,database=db,autocommit=True)
+            self.sql = mysql.connector.connect(
+                host=dbip, user=dbuser, password=dbpw, database=db, autocommit=True
+            )
             self.cur = self.cursor()
         except Exception as e:
             self.sql = None
             print(e)
+
     def cursor(self):
         try:
             self.sql.ping(reconnect=True, attempts=3, delay=5)
         except Exception:
-            if self.sql != None : self.sql.close()  
-            self.sql = mysql.connector.connect(host=dbip,user=dbuser,password=dbpw,database=db,autocommit=True)
+            if self.sql != None:
+                self.sql.close()
+            self.sql = mysql.connector.connect(
+                host=dbip, user=dbuser, password=dbpw, database=db, autocommit=True
+            )
         return self.sql.cursor()
