ea9ad5c7d044a0955bda36e2c3ad293f6ffcc268
Manti <67705577+mantikafasi@users.noreply.github.com>
2022-11-16 23:19:29 +0300

    worst

--- userReviewsManager.py
+++ userReviewsManager.py
@@ -156,7 +156,7 @@ class Manager:
         )
         vals = returnJsonValue(cur, True)
         for review in vals:
-            review["badges"] = self.getBadgesOfUser(review["senderDiscordID"])
+            review["badges"] = self.getBadgesOfUser(review["senderdiscordid"])
         return vals
 
     @cached(cache=TTLCache(maxsize=1024, ttl=2))
