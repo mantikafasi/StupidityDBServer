13f016b876bc315f76206bba65976e9d5b3e37e7
Manti <67705577+mantikafasi@users.noreply.github.com>
2022-11-20 15:49:06 +0300

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -231,7 +231,7 @@ async def sql(ctx, *, query: str):
     except Exception as e:
         await ctx.send(str(e))
 
-@bot.hybrid_command("addBadge")
+@bot.hybrid_command("addbadge")
 async def addBadge(ctx, discordid:int, badgename:str,badgeicon:str,redirecturl:str):
     if not manager.isUserAdminID(ctx.author.id):
         await ctx.send("You are not authrorized to add badges")
