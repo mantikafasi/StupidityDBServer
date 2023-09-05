1d146b54091dd8be7931471d0230273af9642b69
Manti <67705577+mantikafasi@users.noreply.github.com>
2023-09-05 21:58:12 +0300

    remoke token of deleted users

--- UserUpdater.py
+++ UserUpdater.py
@@ -10,7 +10,7 @@ client = discord.Client(intents=discord.Intents.all())
 
 def getUsers():
     cursor = manager.cursor()
-    cursor.execute("SELECT discord_id FROM users")
+    cursor.execute("SELECT discord_id,username FROM users")
     return cursor.fetchall()
 
 
@@ -20,11 +20,18 @@ async def main():
 
     users = getUsers()
     for user in users:
+        if user[1].startswith("Deleted User"):
+            continue
+        
         try:
             user = await fetchUser(user[0])
             if user.id == 1134864775000629298:
                 raise IndentationError("NO DONT UPDATE WARNING USER")
-            updateDBUser(user)
+            
+            if user.name.startswith("Deleted User"):
+                revokeToken(user.id)
+            else:
+                updateDBUser(user)
         except Exception as e:
             print("An Explosion Happened:" + str(e))
     await client.close()
@@ -32,6 +39,12 @@ async def main():
 
 async def fetchUser(userId):
     return await client.fetch_user(userId)
+
+def revokeToken(discordId: string):
+    manager.cursor().execute(
+        "UPDATE users SET token=NULL WHERE discord_id=%s", (discordId,)
+    )
+    print("Revoked Token:" + str(userId))
 
 
 def updateDBUser(user: discord.User):
